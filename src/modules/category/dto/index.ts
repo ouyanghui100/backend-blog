b48aa3fc303761f1d2e28c7f22fbe629d0b01d6e
OuyangHui <2902897795@qq.com>
2025-07-11 16:48:12 +0800

    refactor(分类模块): 更新 DTO 导出，添加分类统计 DTO 并优化导出结构

--- src/modules/category/dto/index.ts
+++ src/modules/category/dto/index.ts
@@ -3,10 +3,10 @@
  */
 
 export { CategoryResponseDto } from './category-response.dto';
+export * from './category-statistics.dto';
 export { CreateCategoryDto } from './create-category.dto';
 export { QueryCategoryDto } from './query-category.dto';
 export { UpdateCategoryDto } from './update-category.dto';
-export * from './category-statistics.dto';
 
 // 从公共模块导出统一的响应格式
 export { ApiResponseDto, StatusCodes } from '../../../common';
